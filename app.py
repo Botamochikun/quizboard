546ca12df0ca2e1e6d5f2654d18f824f6e0dcf66
Botamochikun <shibo.ao0117@gmail.com>
2025-08-02 13:50:45 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+answers = []
+
+@app.route('/')
+def index():
+    return redirect('/player')
+
+@app.route('/player')
+def player():
+    return render_template('player.html')
+
+@app.route('/host')
+def host():
+    return render_template('host.html', answers=answers)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    image = request.form['image']
+    answers.append((name, image))
+    return redirect('/player')
+
+@app.route('/reset')
+def reset():
+    answers.clear()
+    return redirect('/host')
+
+if __name__ == '__main__':
+    app.run(debug=True)
